cb9f2b4116c618799d73468e5b997726d29f1deb
rahulshinde31 <rahul.a.shinde@centurylink.com>
2020-05-06 09:33:37 +0000

    Lab 4

--- main.js
+++ main.js
@@ -7,25 +7,6 @@
   Assign the imported object to variable path.
 */
 const path = require('path');
-
-/**
- * Import helper function module located in the same directory
- * as this module. IAP requires the path object's join method
- * to unequivocally locate the file module.
- */
-const { getIpv4MappedIpv6Address } = require(path.join(__dirname, 'ipv6.js'));
-const IPCIDR = require('ip-cidr');
-
-/**
- * Calculate and return the first host IP address from a CIDR subnet.
- * @param {string} cidrStr - The IPv4 subnet expressed
- *                 in CIDR format.
- * @param {callback} callback - A callback function.
- * @return {string} (firstIpAddress) - An IPv4 address.
- */
-
-
-
 /*
   This section is used to test function and log any errors.
   We will make several positive and negative tests.
@@ -47,37 +28,45 @@ class IpAddress {
         // under Documentation -> Developer Guides -> Log Class Guide
         log.info('Starting the IpAddress product.');
     }
-    getFirstIpAddress(cidrStr, callback) {
+        /**
+        * Calculate and return the first host IP address from a CIDR subnet.
+        * @param {string} cidrStr - The IPv4 subnet expressed
+        *                 in CIDR format.
+        * @param {callback} callback - A callback function.
+        * @return {string} (firstIpAddress) - An IPv4 address.
+        */
+        getFirstIpAddress(cidrStr, callback) {
 
-    // Initialize return arguments for callback
-    let firstIpAddress = null;
-    let callbackError = null;
+        // Initialize return arguments for callback
+        let firstIpAddress = null;
+        let callbackError = null;
 
-    // Instantiate an object from the imported class and assign the instance to variable cidr.
-    const cidr = new IPCIDR(cidrStr);
-    // Initialize options for the toArray() method.
-    // We want an offset of one and a limit of one.
-    // This returns an array with a single element, the first host address from the subnet.
-    const options = {
-        from: 1,
-        limit: 1
-    };
+        // Instantiate an object from the imported class and assign the instance to variable cidr.
+        const cidr = new IPCIDR(cidrStr);
+        // Initialize options for the toArray() method.
+        // We want an offset of one and a limit of one.
+        // This returns an array with a single element, the first host address from the subnet.
+        const options = {
+            from: 1,
+            limit: 1
+        };
 
-    // Use the object's isValid() method to verify the passed CIDR.
-    if (!cidr.isValid()) {
-        // If the passed CIDR is invalid, set an error message.
-        callbackError = 'Error: Invalid CIDR passed to getFirstIpAddress.';
-    } else {
-        // If the passed CIDR is valid, call the object's toArray() method.
-        // Notice the destructering assignment syntax to get the value of the first array's element.
-        [firstIpAddress] = cidr.toArray(options);
-    }
-    // Call the passed callback function.
-    // Node.js convention is to pass error data as the first argument to a callback.
-    // The IAP convention is to pass returned data as the first argument and error
-    // data as the second argument to the callback function.
-    return callback(firstIpAddress, callbackError);
-    }
+        // Use the object's isValid() method to verify the passed CIDR.
+        if (!cidr.isValid()) {
+            // If the passed CIDR is invalid, set an error message.
+            callbackError = 'Error: Invalid CIDR passed to getFirstIpAddress.';
+        } else {
+            // If the passed CIDR is valid, call the object's toArray() method.
+            // Notice the destructering assignment syntax to get the value of the first array's element.
+            [firstIpAddress] = cidr.toArray(options);
+        }
+        // Call the passed callback function.
+        // Node.js convention is to pass error data as the first argument to a callback.
+        // The IAP convention is to pass returned data as the first argument and error
+        // data as the second argument to the callback function.
+        return callback(firstIpAddress, callbackError);
+        }
+
 
 }
 
